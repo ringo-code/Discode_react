d0a80471e57763f5e1811697be9f63f6622ce39d
ringo-git <saya@nishisakahayayanoMacBook-Air.local>
2025-01-10 00:03:36 +0900

    サイドバーコンポーネント作成&CSS追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
+import Sidebar from './components/sidebar/Sidebar'
 
 function App() {
   return (
       <div className="App">
-        <h1>discode</h1>
+        {/* sidebar */}
+        <Sidebar />
+
+        {/* home */}
       </div>
   )
 }
