d3dfc9c19c07786be3afcea6e456e28bbd58192e
ringo-git <saya@nishisakahayayanoMacBook-Air.local>
2025-01-11 18:11:23 +0900

    チャットコンポーネント作成

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
+import { Chat } from './components/chat/Chat'
 import Sidebar from './components/sidebar/Sidebar'
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
         {/* sidebar */}
         <Sidebar />
 
-        {/* home */}
+        {/* chat */}
+        <Chat />
       </div>
   )
 }
---
+++ src/components/chat/Chat.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Chat.scss'
+
+export const Chat = () => {
+    return (
+        <div className="chat">
+            {/* chatHeader */}
+            {/* chatMessage */}
+            {/* chatInput */}
+        </div>
+    )
+}
