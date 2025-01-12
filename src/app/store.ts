8609e0475bd243b6de0f0ac0c4443c454e4015c5
ringo-git <saya@nishisakahayayanoMacBook-Air.local>
2025-01-12 17:54:31 +0900

    Typescriptで初期状態管理＆Providerでstoreを使用可能にする

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { useReducer } from 'react'
+import useReducer from '../features/userSlice'
 
 export const store = configureStore({
     reducer: useReducer,
