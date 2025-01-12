647fa33008c1df7ad0668ae46dc00985f8c32c77
ringo-git <saya@nishisakahayayanoMacBook-Air.local>
2025-01-12 18:26:07 +0900

    ログイン用ページ作成

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
 import { Chat } from './components/chat/Chat'
+import Login from './components/login/Login'
 import Sidebar from './components/sidebar/Sidebar'
 
 function App() {
+  // const user = userSelector((state) => state.user.user);
+  const user = null;
+
   return (
       <div className="App">
-        {/* sidebar */}
-        <Sidebar />
-
-        {/* chat */}
-        <Chat />
+        {user ? (
+          <>
+            <Sidebar />
+            <Chat />
+          </>
+        ):(
+          <>
+            <Login />
+          </>
+        )}
       </div>
-  )
+  );
 }
 
 export default App
