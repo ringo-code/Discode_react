1c32925b985241871309d456dc8ba9939252e364
ringo-git <saya@nishisakahayayanoMacBook-Air.local>
2025-01-15 18:25:53 +0900

    ログアウト&サイドバーにユーザー情報反映

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -6,8 +6,12 @@ import SidebarChannel from './SidebarChannel';
 import MicIcon from '@mui/icons-material/Mic';
 import HeadphonesIcon from '@mui/icons-material/Headphones';
 import SettingsIcon from '@mui/icons-material/Settings';
+import { auth } from '../../firebase';
+import { userAppSelector } from '../../app/hooks';
 
 const Sidebar = () => {
+  const user = userAppSelector((state) => state.user);
+
   return (
     <div className="sidebar">
       {/* sidebarLeft */}
@@ -46,10 +50,10 @@ const Sidebar = () => {
             <div className="sidebarFooter">
               {/* sidebarAccount */}
               <div className="sidebarAccount">
-                <img src="./icon.png" alt="" />
+                <img src={user?.photo} alt="" onClick={() => auth.signOut()}/>
                 <div className="accountName">
-                  <h4>SibaCode</h4>
-                  <span>#8162</span>
+                  <h4>{user?.displayName}</h4>
+                  <span>#{user?.uid.substring(0, 4)}</span>
                 </div>
               </div>
 
