1a7e2dd52e87ea3721132b423e5f5783bc53b4b4
ringo-git <saya@nishisakahayayanoMacBook-Air.local>
2025-01-15 13:08:05 +0900

    ログインしたユーザー情報をstoreに通知して状態更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { useEffect } from "react";
 import './App.scss'
-import { userAppSelector } from './app/hooks'
+import { userAppSelector, useAppDispatch } from './app/hooks'
+import { login, logout } from "./features/userSlice";
 import { Chat } from './components/chat/Chat'
 import Login from './components/login/Login'
 import Sidebar from './components/sidebar/Sidebar'
+import { auth } from './firebase'
 
 function App() {
   const user = userAppSelector((state) => state.user);
-  console.log(user);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((loginUser) => {
+      if(loginUser) {
+        dispatch(
+          login({
+            uid: loginUser.uid,
+            photo: loginUser.photoURL,
+            email: loginUser.email,
+            displayName: loginUser.displayName,
+          })
+        )
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]); // ２回目以降も更新されるようdispatchを設定
 
   return (
       <div className="App">
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import './Login.scss'
 import { Button } from '@mui/material';
+import { signInWithPopup } from 'firebase/auth';
+import { auth, provider } from '../../firebase';
 
 const Login = () => {
+
+    const signIn = () => {
+        // ここでgoogleログインの処理ができる
+        signInWithPopup(auth, provider).catch((err) => {
+            alert(err.message);
+        });
+    };
+
     return  (
         <div className="login">
             <div className="loginLogo">
                 <img src="./discordLogin.png" alt="" />
             </div>
 
-            <Button>ログイン</Button>
+            <Button onClick={signIn}>ログイン</Button>
         </div>
     );
 };
